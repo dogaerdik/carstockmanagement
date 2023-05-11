58da7a06868e6a9521cf9b58f9babe063bcef57a
dogaerdik <dogaerdik@hotmail.com>
2023-05-11 05:27:56 +0200

    targets removed

--- car-server/target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-insert into car values(1, 500, CURRENT_DATE(), 0.80, 'Mazda', 'Mazda3' , 14000 , 'Sport Package' , 2014);
-insert into car values(2, 500, CURRENT_DATE(), 0.65, 'Ford', 'Focus' , 8000 , 'Sport Package' , 2008);
-insert into car values(3, 500, CURRENT_DATE(), 0.90, 'Mercedes-Benz', 'Gla 200' , 25000 , 'Sport Package' , 2020);
-insert into car values(4, 500, CURRENT_DATE(), 1.00, 'BMW', 'BMW 1 Series' , 18000 , 'Full Package' , 2016);
-insert into car values(5, 500, CURRENT_DATE(), 0.60, 'Kia', 'Rio' , 18000 , 'Basic Package' , 2020);
